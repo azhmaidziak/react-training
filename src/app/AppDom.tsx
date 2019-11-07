5b5b068047d571b436ec0cae55f558a43e413134
Aliaksandr Zhmaidziak <Aliaksandr.Zhmaidziak@smarsh.com>
2019-11-07 16:37:40 +0300

    task-2-webpack remove multiple empty lines in webpack.frontend.js

--- src/app/AppDom.tsx
+++ src/app/AppDom.tsx
@@ -4,12 +4,6 @@ import BodyDom from "../body";
 import {Page} from "../models";
 
 export default class AppDom extends React.Component<Page> {
-    constructor(page:Page){
-        super(page);
-        this.state = {
-            count: 0
-        }
-    }
 
     render() {
         return <React.Fragment>
